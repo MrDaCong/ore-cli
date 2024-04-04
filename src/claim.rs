63109e4672c3cab8084b90c6150aa2dba9d733de
MrDaCong <wangcong0816@foxmail.com>
2024-04-04 16:06:55 +0800

    Update claim.rs
    WebSocketRpcClient

--- src/claim.rs
+++ src/claim.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use ore::{self, state::Proof, utils::AccountDeserialize};
-use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_client::nonblocking::websocket::WebSocketRpcClient;
 use solana_program::pubkey::Pubkey;
 use solana_sdk::{
     commitment_config::CommitmentConfig, compute_budget::ComputeBudgetInstruction,
@@ -18,7 +18,7 @@ impl Miner {
     pub async fn claim(&self, cluster: String, beneficiary: Option<String>, amount: Option<f64>) {
         let signer = self.signer();
         let pubkey = signer.pubkey();
-        let client = RpcClient::new_with_commitment(cluster, CommitmentConfig::confirmed());
+        let client = WebSocketRpcClient::new_with_commitment(cluster, CommitmentConfig::confirmed());
         let beneficiary = match beneficiary {
             Some(beneficiary) => {
                 Pubkey::from_str(&beneficiary).expect("Failed to parse beneficiary address")
@@ -62,7 +62,7 @@ impl Miner {
         // Initialize client.
         let signer = self.signer();
         let client =
-            RpcClient::new_with_commitment(self.cluster.clone(), CommitmentConfig::confirmed());
+            WebSocketRpcClient::new_with_commitment(self.cluster.clone(), CommitmentConfig::confirmed());
 
         // Build instructions.
         let token_account_pubkey = spl_associated_token_account::get_associated_token_address(
