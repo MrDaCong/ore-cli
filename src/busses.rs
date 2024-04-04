2c688b51ba173a0c4133b80c858110e765334750
MrDaCong <wangcong0816@foxmail.com>
2024-04-04 16:06:09 +0800

    Update busses.rs
    WebSocketRpcClient

--- src/busses.rs
+++ src/busses.rs
@@ -1,5 +1,5 @@
 use ore::{state::Bus, utils::AccountDeserialize, BUS_ADDRESSES};
-use solana_client::{client_error::Result, nonblocking::rpc_client::RpcClient};
+use solana_client::{client_error::Result, nonblocking::websocket::WebSocketRpcClient};
 use solana_sdk::commitment_config::CommitmentConfig;
 
 use crate::Miner;
@@ -7,7 +7,7 @@ use crate::Miner;
 impl Miner {
     pub async fn busses(&self) {
         let client =
-            RpcClient::new_with_commitment(self.cluster.clone(), CommitmentConfig::confirmed());
+            WebSocketRpcClient::new_with_commitment(self.cluster.clone(), CommitmentConfig::confirmed());
         for address in BUS_ADDRESSES.iter() {
             let data = client.get_account_data(address).await.unwrap();
             match Bus::try_from_bytes(&data) {
@@ -21,7 +21,7 @@ impl Miner {
 
     pub async fn get_bus(&self, id: usize) -> Result<Bus> {
         let client =
-            RpcClient::new_with_commitment(self.cluster.clone(), CommitmentConfig::confirmed());
+            WebSocketRpcClient::new_with_commitment(self.cluster.clone(), CommitmentConfig::confirmed());
         let data = client.get_account_data(&BUS_ADDRESSES[id]).await?;
         Ok(*Bus::try_from_bytes(&data).unwrap())
     }
